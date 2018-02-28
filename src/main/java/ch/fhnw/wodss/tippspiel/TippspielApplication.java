850b8b42f3776083c1b6923524672bd274d5be7e
Yanick Schraner <yanick.schraner@gmail.com>
2018-02-28 09:29:56 +0100

    Add gradle and Spring

---
+++ src/main/java/ch/fhnw/wodss/tippspiel/TippspielApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.wodss.tippspiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TippspielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TippspielApplication.class, args);
+	}
+}
