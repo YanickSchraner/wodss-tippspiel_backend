bfa457849152ac77174596e9d4f74368a3f4bb23
Yanick Schraner <yanick.schraner@gmail.com>
2018-02-28 13:30:08 +0100

    Add example Model

--- src/main/java/ch/fhnw/wodss/tippspiel/Domain/TippGroup.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/Domain/TippGroup.java
@@ -1,4 +1,51 @@
 package ch.fhnw.wodss.tippspiel.Domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class TippGroup {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column
+    private Long id;
+
+    @Column
+    @NotNull
+    @Size(min = 1, max = 100)
+    private String name;
+
+    @Column
+    @NotNull
+    @Size(min = 10, max = 300)
+    private String password;
+
+    @Column
+    @NotNull
+    @Min(0)
+    @Max(Integer.MAX_VALUE)
+    private Integer score;
+
+    @Column
+    @ManyToMany(fetch = FetchType.EAGER)
+    List<User> members;
+
+    public TippGroup(Long id, String name, String password, Integer score, List<User> members){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.score = score;
+        this.members = members;
+    }
 }
