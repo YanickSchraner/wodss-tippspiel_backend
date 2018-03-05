34b6c69fd7b844e08cd1a011b6a5d76fad4cdcce
Tom <tom.ohme@gmx.ch>
2018-03-05 17:45:53 +0100

    Implement persistance

--- src/main/java/ch/fhnw/wodss/tippspiel/Persistance/TournamentTeamRepository.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/Persistance/TournamentTeamRepository.java
@@ -4,4 +4,7 @@ import ch.fhnw.wodss.tippspiel.Domain.TournamentTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TournamentTeamRepository extends JpaRepository<TournamentTeam, Long> {
+
+    TournamentTeam getTournamentTeamByName(String name);
+
 }
