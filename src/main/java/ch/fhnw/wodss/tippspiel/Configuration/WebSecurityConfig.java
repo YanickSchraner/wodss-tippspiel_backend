a91958f01c6ffd1c615506bf2dd65eb533c4b548
Yanick Schraner <yanick.schraner@gmail.com>
2018-03-07 13:17:33 +0100

    Initial security configuration

--- src/main/java/ch/fhnw/wodss/tippspiel/Configuration/WebSecurityConfig.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/Configuration/WebSecurityConfig.java
@@ -1,4 +1,30 @@
 package ch.fhnw.wodss.tippspiel.Configuration;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider()
+    }
 }
