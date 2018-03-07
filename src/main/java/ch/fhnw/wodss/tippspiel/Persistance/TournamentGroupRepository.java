ebeaf006092f1fcdcca792e6096d4ed1fc5bb641
Tom <tom.ohme@gmx.ch>
2018-03-07 16:38:53 +0100

    Add search query for tournament group persistance

--- src/main/java/ch/fhnw/wodss/tippspiel/Persistance/TournamentGroupRepository.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/Persistance/TournamentGroupRepository.java
@@ -3,5 +3,10 @@ package ch.fhnw.wodss.tippspiel.Persistance;
 import ch.fhnw.wodss.tippspiel.Domain.TournamentGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TournamentGroupRepository extends JpaRepository<TournamentGroup, Long> {
+
+    Optional<TournamentGroup> getTournamentGroupByName(String name);
+
 }
