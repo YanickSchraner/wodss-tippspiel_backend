8bdc344fd0803a4f5f0a91c8deef240b7bb0117d
Yanick Schraner <yanick.schraner@gmail.com>
2018-03-08 16:01:33 +0100

    Configure application.properties

--- src/main/java/ch/fhnw/wodss/tippspiel/TippspielApplication.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/TippspielApplication.java
@@ -3,9 +3,11 @@ package ch.fhnw.wodss.tippspiel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAspectJAutoProxy
 //@EnableScheduling
 public class TippspielApplication {
 
