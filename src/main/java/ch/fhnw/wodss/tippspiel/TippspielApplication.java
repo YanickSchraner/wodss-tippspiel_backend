850b4619e656193346dcc1d2c27512cede996fee
Yanick Schraner <yanick.schraner@gmail.com>
2018-04-02 14:09:42 +0200

    Enable ehcache

--- src/main/java/ch/fhnw/wodss/tippspiel/TippspielApplication.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/TippspielApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAspectJAutoProxy
-//@EnableScheduling
+@EnableScheduling
 public class TippspielApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TippspielApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TippspielApplication.class, args);
+    }
+
 }
