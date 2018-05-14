cb87b57655f6cda9abaa8554a0a6e9acc17c4c29
Yanick Schraner <yanick.schraner@gmail.com>
2018-05-14 18:18:05 +0200

    Add cors headers manualy

--- src/main/java/ch/fhnw/wodss/tippspiel/configuration/WebSecurityConfig.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/configuration/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -64,7 +63,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and().anonymous().disable()
