32de54382dafae09fd0d05f0979fcee3c0c37858
Yanick Schraner <yanick.schraner@gmail.com>
2018-05-14 18:05:43 +0200

    Add cors headers manualy

--- src/main/java/ch/fhnw/wodss/tippspiel/configuration/EnforceCorsFilter.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/configuration/EnforceCorsFilter.java
@@ -18,6 +18,10 @@ public class EnforceCorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
             response.setStatus(HttpServletResponse.SC_OK);
         }
         if (!CorsUtils.isCorsRequest(request)) {
