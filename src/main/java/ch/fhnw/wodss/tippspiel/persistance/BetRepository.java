a1c12fa1047ecb99106089729a17e7f013a6217b
Yanick Schraner <yanick.schraner@gmail.com>
2018-05-21 16:58:13 +0200

    New methods for mailer and scorer

--- src/main/java/ch/fhnw/wodss/tippspiel/persistance/BetRepository.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/persistance/BetRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BetRepository extends JpaRepository<Bet, Long> {
@@ -13,6 +14,15 @@ public interface BetRepository extends JpaRepository<Bet, Long> {
 
     boolean existsBetByUser_IdAndGame_Id(Long userId, Long gameId);
 
-    @Query("SELECT * FROM Bet b WHERE b.idUser = :betUser")
+    @Query("SELECT b FROM Bet b WHERE b.user = :betUser")
     List<Bet> getBetsForUser(@Param("betUser") Long userId);
+
+    @Query("SELECT b FROM Bet b WHERE b.game.dateTime BETWEEN :start AND :end")
+    List<Bet> getTodayBets(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+
+    @Query("SELECT COUNT(b.score) FROM Bet b WHERE b.user = :user AND  b.game.dateTime BETWEEN :start AND :end")
+    int getTodayScore(@Param("user") Long userId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+
+    @Query("SELECT COUNT(b.score) FROM Bet b WHERE  b.user = :user")
+    int getUserScore(@Param("user") Long userId);
 }
