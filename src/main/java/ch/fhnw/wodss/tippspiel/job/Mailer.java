fa941946e376249cb561264fac6c5924ba111632
Yanick Schraner <yanick.schraner@gmail.com>
2018-05-21 17:29:44 +0200

    Implement mailer and scorer scheduled task

--- src/main/java/ch/fhnw/wodss/tippspiel/job/Mailer.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/job/Mailer.java
@@ -1,6 +1,9 @@
 package ch.fhnw.wodss.tippspiel.job;
 
+import ch.fhnw.wodss.tippspiel.domain.Game;
 import ch.fhnw.wodss.tippspiel.domain.User;
+import ch.fhnw.wodss.tippspiel.persistance.BetRepository;
+import ch.fhnw.wodss.tippspiel.persistance.GameRepository;
 import ch.fhnw.wodss.tippspiel.persistance.UserRepository;
 import ch.fhnw.wodss.tippspiel.util.GMail;
 import com.google.api.services.gmail.Gmail;
@@ -9,31 +12,70 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class Mailer {
     private static final Logger logger = LoggerFactory.getLogger(Mailer.class);
 
-    @Autowired
-    UserRepository repository;
+    private UserRepository userRepository;
+    private GameRepository gameRepository;
+    private BetRepository betRepository;
+
     @Value("${mailer.from}")
     private String from;
 
-    @Scheduled(cron = "0 8 * * *")
+    @Autowired
+    public Mailer(UserRepository userRepository, GameRepository gameRepository, BetRepository betRepository) {
+        this.userRepository = userRepository;
+        this.gameRepository = gameRepository;
+        this.betRepository = betRepository;
+    }
+
+    @Scheduled(cron = "0 0 8 * * *")
     public void remindUsersToBet() throws GeneralSecurityException {
-        List<User> usersToRemind = repository.findAllByRemindersTrue();
+        List<User> usersToRemind = userRepository.findAllByRemindersTrue();
         Gmail service = GMail.getAuthorizedService().orElseThrow(() -> new GeneralSecurityException("Can't authenticate to gmail service."));
-        String message = "Vergessen Sie nicht Ihren Tipp für die heutigen Spiele abzugeben!";
+        String message = "Vergiss nicht deinen Tipp für die heutigen Spiele abzugeben!";
         String subject = "Tippspiel WM 2018 - Ausstehende Tippabgaben!";
-        for (User user: usersToRemind) {
+        for (User user : usersToRemind) {
             try {
-                MimeMessage mimeMessage = GMail.createEmail(user.getEmail(), from, subject, message);
+                if (checkUserHasOpenBets(user)) {
+                    MimeMessage mimeMessage = GMail.createEmail(user.getEmail(), from, subject, message);
+                    GMail.sendMessage(service, "me", mimeMessage);
+                }
+            } catch (MessagingException | IOException e) {
+                logger.error(Arrays.toString(e.getStackTrace()));
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 23 * * *")
+    public void sendDailyReport() throws GeneralSecurityException {
+        LocalDateTime start = LocalDateTime.now().withHour(0).withMinute(0);
+        LocalDateTime end = LocalDateTime.now().withHour(23).withMinute(59);
+        Gmail service = GMail.getAuthorizedService().orElseThrow(() -> new GeneralSecurityException("Can't authenticate to gmail service."));
+        StringBuilder message = new StringBuilder();
+        message.append("Du hast heute ");
+        String subject = "Tippspiel WM 2018 - Tagesrapport";
+        List<User> usersToReport = userRepository.findAllByDailyResultsTrue();
+        for (User userToReport : usersToReport) {
+            int score = betRepository.getTodayScore(userToReport, start, end);
+            message.append(score);
+            message.append(" Punkte erzielt!\n");
+            message.append("Aktuell hast du ");
+            message.append(betRepository.getUserScore(userToReport));
+            message.append(" Punke.\n Herzliche Gratulation!");
+            try {
+                MimeMessage mimeMessage = GMail.createEmail(userToReport.getEmail(), from, subject, message.toString());
                 GMail.sendMessage(service, "me", mimeMessage);
             } catch (MessagingException | IOException e) {
                 logger.error(Arrays.toString(e.getStackTrace()));
@@ -41,13 +83,14 @@ public class Mailer {
         }
     }
 
-    @Scheduled(cron = "0 23 * * *")
-    public void sendDailyReport(){
-        List<User> userToReport = repository.findAllByDailyResultsTrue();
-
-    }
-
-    private boolean checkUserHasOpenBets(User user){
-        return true;
+    private boolean checkUserHasOpenBets(User user) {
+        LocalDateTime start = LocalDateTime.now().withHour(0).withMinute(0);
+        LocalDateTime end = LocalDateTime.now().withHour(23).withMinute(59);
+        List<Game> todayGames = gameRepository.getAllByDateTimeIsBetween(start, end);
+        int bettedGames = 0;
+        for (Game game : todayGames) {
+            bettedGames += user.getBets().stream().filter(bet -> bet.getGame().equals(game)).count();
+        }
+        return bettedGames < todayGames.size();
     }
 }
--- src/main/java/ch/fhnw/wodss/tippspiel/persistance/BetRepository.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/persistance/BetRepository.java
@@ -1,6 +1,7 @@
 package ch.fhnw.wodss.tippspiel.persistance;
 
 import ch.fhnw.wodss.tippspiel.domain.Bet;
+import ch.fhnw.wodss.tippspiel.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,14 +16,14 @@ public interface BetRepository extends JpaRepository<Bet, Long> {
     boolean existsBetByUser_IdAndGame_Id(Long userId, Long gameId);
 
     @Query("SELECT b FROM Bet b WHERE b.user = :betUser")
-    List<Bet> getBetsForUser(@Param("betUser") Long userId);
+    List<Bet> getBetsForUser(@Param("betUser") User user);
 
     @Query("SELECT b FROM Bet b WHERE b.game.dateTime BETWEEN :start AND :end")
     List<Bet> getTodayBets(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("SELECT COUNT(b.score) FROM Bet b WHERE b.user = :user AND  b.game.dateTime BETWEEN :start AND :end")
-    int getTodayScore(@Param("user") Long userId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    int getTodayScore(@Param("user") User user, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("SELECT COUNT(b.score) FROM Bet b WHERE  b.user = :user")
-    int getUserScore(@Param("user") Long userId);
+    int getUserScore(@Param("user") User user);
 }
