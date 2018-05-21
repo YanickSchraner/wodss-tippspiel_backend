640b2959b6c46b4a696844221a6ced2e2bd14a63
Yanick Schraner <yanick.schraner@gmail.com>
2018-05-21 17:53:23 +0200

    Fix issue in scorer

--- src/main/java/ch/fhnw/wodss/tippspiel/job/Scorer.java
+++ src/main/java/ch/fhnw/wodss/tippspiel/job/Scorer.java
@@ -7,6 +7,8 @@ import ch.fhnw.wodss.tippspiel.persistance.BetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,7 +25,8 @@ public class Scorer {
         this.betGroupRepository = betGroupRepository;
     }
 
-    @Scheduled(cron = "0 0 23 * * *")
+    //@Scheduled(cron = "0 0 23 * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void setBetScore() {
         LocalDateTime start = LocalDateTime.now().withHour(0).withMinute(0);
         LocalDateTime end = LocalDateTime.now().withHour(23).withMinute(59);
@@ -58,11 +61,14 @@ public class Scorer {
         return score;
     }
 
-    @Scheduled(cron = "0 10 23 * * *")
+    //@Scheduled(cron = "0 10 23 * * *")
+    @Scheduled(cron = "0 * * * * *")
+    @Transactional(propagation = Propagation.REQUIRED)
     public void updateBetGroupScore() {
         for (BetGroup betGroup : betGroupRepository.findAll()) {
-            double score = betGroup.getMembers().stream().mapToInt(user -> user.getBets().stream().mapToInt(Bet::getScore).sum()).average().getAsDouble();
+            double score = betGroup.getMembers().stream().mapToInt(user -> user.getBets().stream().mapToInt(Bet::getScore).sum()).average().orElse(0);
             betGroup.setScore((int) score);
+            betGroupRepository.save(betGroup);
         }
     }
 }
